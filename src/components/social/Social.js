37941a71a7f0c10a13d05a7d4b4c238e4f5f272e
mfabdu <mfabdu@outlook.com>
2020-10-27 16:23:53 +0300

    add social icons

--- src/components/social/Social.js
+++ src/components/social/Social.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
-import FontAwesomeIcon from "@fortawesome/react-fontawesome"
-import * as Icons from "@fortawesome/fontawesome-free-solid"
-
+import React, { Component } from "react";
+import { FaLinkedin, FaFacebook, FaTwitter, FaGithub } from "react-icons/fa";
 
 class Social extends Component {
-  render () {
+  state = {
+    linkedin: "https://www.linkedin.com/in/mohamed-fawzi-abdel-hameed-753425111/",
+    facebook: "https://www.linkedin.com/in/mohamed-fawzi-abdel-hameed-753425111/",
+    twitter: "https://www.linkedin.com/in/mohamed-fawzi-abdel-hameed-753425111/",
+    github: "https://www.linkedin.com/in/mohamed-fawzi-abdel-hameed-753425111/",
+  };
+  render() {
     return (
       <div>
-        <FontAwesomeIcon icon={Icons.faCopyright} size="6x" />
+        <ul>
+          <li>
+            <a href={this.state.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+          <li>
+            <a href={this.state.facebook}>
+              <FaFacebook />
+            </a>
+          </li>
+          <li>
+            <a href={this.state.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href={this.state.github}>
+              <FaGithub />
+            </a>
+          </li>
+        </ul>
       </div>
-    )
+    );
   }
 }
 
-export default Social
+export default Social;
