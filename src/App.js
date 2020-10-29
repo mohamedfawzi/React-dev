5b80be3f130246a538d5a96403e68de2c6777a9d
mfabdu <mfabdu@outlook.com>
2020-10-29 16:36:59 +0300

    face

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { Component } from 'react';
-import Header from './components/header/Header';
-import GetFacebook from './components/getFacebook/GetFacebook';
-//import bootstrap 
-import 'bootstrap/dist/css/bootstrap.css';
-
+import React, { Component } from 'react'
+import Header from './components/header/Header'
+import GetFacebook from './components/getFacebook/GetFacebook'
+import './App.scss'
+// import bootstrap 
+import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className='wrapper'>
-        <Header hrefPath = "My Home" />
-        <GetFacebook />
-
+        <Header hrefPath='My Home' />
+        <div className='container-fluid'>
+          <div className='row'>
+            <div className='col-sm-3'>
+              <GetFacebook />
+            </div>
+            
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/getFacebook/GetFacebook.js
+++ src/components/getFacebook/GetFacebook.js
@@ -1,12 +1,17 @@
-import React, { Component} from 'react';
-import { FacebookProvider, EmbeddedPost } from 'react-facebook';
- 
+import React, { Component } from 'react'
+import { FacebookProvider, Page } from 'react-facebook'
+import { FaLinkedin, FaFacebook, FaTwitter, FaGithub } from "react-icons/fa";
+
 export default class GetFacebook extends Component {
-  render() {
+    
+  render () {     
     return (
-      <FacebookProvider appId="381216016410763">
-        <EmbeddedPost  href="http://www.facebook.com" width="500" />
+      <FacebookProvider appId='381216016410763'>
+        <div className='bg-secondary p-3'>
+            <FaFacebook />
+        </div>
+        <Page href='https://www.facebook.com/wrshaa/' style={{width: "100%"}} tabs='timeline' />
       </FacebookProvider>
-    );
+    )
   }
 }
